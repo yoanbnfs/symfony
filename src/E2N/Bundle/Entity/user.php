74422404c5b6ad0fed2f4a28d28f807d9fa6f12d
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2018-03-19 11:18:09 +0100

    partie 2

---
+++ src/E2N/Bundle/Entity/user.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace E2N\Bundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * user
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="E2N\Bundle\Repository\userRepository")
+ */
+class user
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createDate", type="datetime")
+     */
+    private $createDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="motivation", type="text")
+     */
+    private $motivation;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="palme", type="integer")
+     */
+    private $palme;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function __construct()
+  {
+    // Par défaut, la date de l'annonce est la date d'aujourd'hui
+    $this->date = new \Datetime();
+  }  
+  // ...
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return user
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return user
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set createDate
+     *
+     * @param \DateTime $createDate
+     *
+     * @return user
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+
+    /**
+     * Get createDate
+     *
+     * @return \DateTime
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return user
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set motivation
+     *
+     * @param string $motivation
+     *
+     * @return user
+     */
+    public function setMotivation($motivation)
+    {
+        $this->motivation = $motivation;
+
+        return $this;
+    }
+
+    /**
+     * Get motivation
+     *
+     * @return string
+     */
+    public function getMotivation()
+    {
+        return $this->motivation;
+    }
+
+    /**
+     * Set palme
+     *
+     * @param integer $palme
+     *
+     * @return user
+     */
+    public function setPalme($palme)
+    {
+        $this->palme = $palme;
+
+        return $this;
+    }
+
+    /**
+     * Get palme
+     *
+     * @return int
+     */
+    public function getPalme()
+    {
+        return $this->palme;
+    }
+}
+
